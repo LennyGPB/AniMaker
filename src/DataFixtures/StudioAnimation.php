f3b220a52dd51c238256d9e3b760039b828ba71b
RakotomananaCedric <cedric.rakotomanana2511@gmail.com>
2023-11-06 16:00:05 +0100

    Fixture Studio 06/11/2023

---
+++ src/DataFixtures/StudioAnimation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\StudioDanimation;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+;
+
+class StudioAnimation extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker=Factory::create('fr_FR');
+        for($i=1;$i<=100;$i++)
+        {
+            $studio=new StudioDanimation();
+            $studio->setNomStudio($faker->words(2, true));
+        }
+        $manager->flush();
+    }
+}
