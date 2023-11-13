b533b12970ccf0fcfe09583ab0f11bbe758c4b0d
RakotomananaCedric <cedric.rakotomanana2511@gmail.com>
2023-11-13 14:37:48 +0100

    La vue de studio d'animation 13/11/2023

---
+++ src/Controller/StudioAnimationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StudioDanimationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudioAnimationController extends AbstractController
+{
+    #[Route('/studio_animation', name: 'studioAnimation', methods:"GET")]
+    public function ListeStudioAnimation(StudioDanimationRepository $repo): Response
+    {
+        $listeStudioAnimation=$repo->findAll();
+        return $this->render('studioAnimation/listeStudio.html.twig', [
+            'listeStudioAnimation' => $listeStudioAnimation,
+        ]);
+    }
+}
