73fc9567a0a2c44b70f718aca9f6f019a18ba333
RakotomananaCedric <cedric.rakotomanana2511@gmail.com>
2023-11-20 15:38:12 +0100

    L'admin du studio d'animation 20/11/2023

--- src/Controller/Admin/StudioAnimationController.php
+++ src/Controller/Admin/StudioAnimationController.php
@@ -1,26 +1,26 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
-use App\Repository\StudioDanimationRepository;
 use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use App\Repository\StudioDanimationRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StudioAnimationController extends AbstractController
 {
-    #[Route('/studio_animation', name: 'studioAnimation', methods:'GET')]
+    #[Route('/admin/studio_animation', name: 'admin_studioAnimation', methods:'GET')]
     public function ListeStudioAnimation(StudioDanimationRepository $repo, PaginatorInterface $paginator, Request $request): Response
     {
         $listeStudioAnimation = $paginator->paginate(
             $repo->listeStudioAnimationComplete(), /* query NOT result */
             $request->query->getInt('page', 1), /*page number*/
-            16 /*limit per page*/
+            10 /*limit per page*/
         );
        
-        return $this->render('studioAnimation/listeStudio.html.twig', [
+        return $this->render('admin/studioAnimation/listeStudio.html.twig', [
             'listeStudioAnimation' => $listeStudioAnimation,
         ]);
     }
